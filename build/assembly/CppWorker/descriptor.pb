bef878342d468364647309f7bef735a84e0d9452
Jacques Elliott <jge29@cam.ac.uk>
2017-03-05 02:55:53 +0000

    initial commit

---
+++ build/assembly/CppWorker/descriptor.pb
@@ -0,0 +1,5 @@
+
+MICppWorker@Windows.exe${IMPROBABLE_WORKER_ID}"
+CppWorker@Windows.zip
+A=CppWorker@Mac${IMPROBABLE_WORKER_ID}"
+CppWorker@Mac.zip
---
+++ workers/unity/Assets/EntityTemplates/ExampleEntityTemplate.cs
@@ -0,0 +1,37 @@
+﻿using Improbable.General;
+using Improbable.Worker;
+using Improbable.Math;
+using Improbable.Player;
+using Improbable.Unity.Core.Acls;
+using UnityEngine;
+
+namespace Assets.EntityTemplates
+{
+    public class ExampleEntityTemplate : MonoBehaviour
+    {
+        // Template definition for a Example entity
+        public static SnapshotEntity GenerateExampleSnapshotEntityTemplate()
+        {
+            // Set name of Unity prefab associated with this entity
+            var exampleEntity = new SnapshotEntity { Prefab = "ExampleEntity" };
+
+            // Define components attached to snapshot entity
+            exampleEntity.Add(new WorldTransform.Data(new WorldTransformData(new Coordinates(0, 0, 0))));
+            exampleEntity.Add(new UnityName.Data(new UnityNameData("your_example_entity")));
+            exampleEntity.Add(new CppName.Data(new CppNameData("your_example_entity")));
+
+            var acl = Acl.Build()
+                // Both FSim (server) workers and client workers granted read access over all states
+                .SetReadAccess(Acl.MakeRequirementSet(CommonAttributeSets.Physics, CommonAttributeSets.Visual, Acl.MakeAttributeSet("cpp")))
+                // Only FSim workers granted write access over WorldTransform component
+                .SetWriteAccess<WorldTransform>(CommonRequirementSets.PhysicsOnly)
+                // Only client workers granted write access over Name component
+                .SetWriteAccess<UnityName>(CommonRequirementSets.VisualOnly)
+                .SetWriteAccess<CppName>(Acl.MakeRequirementSet(Acl.MakeAttributeSet("cpp")));
+
+            exampleEntity.SetAcl(acl);
+
+            return exampleEntity;
+        }
+    }
+}
